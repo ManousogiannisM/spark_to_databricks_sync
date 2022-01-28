f043df2166e4d2cd5264b321b4568aacfdd2ff0c
ManousogiannisM <manousogm@gmail.com>
2022-01-28 15:34:55 +0100

    trying access config files

--- job.py
+++ job.py
@@ -2,3 +2,8 @@ from utils.printers import printer_basic
 
 a = printer_basic()
 print(a)
+
+import json
+with open('sample.json','r') as config_file:
+    config = json.load(config_file)
+print(config['job_name'])
